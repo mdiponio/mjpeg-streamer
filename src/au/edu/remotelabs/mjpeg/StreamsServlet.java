c95b11fc5648c0b8d2b75440e6ac1ae156a5f786
Michael Diponio <michael.diponio@uts.edu.au>
2016-05-18 17:01:59 +1000

    Added ignore project files.

--- src/au/edu/remotelabs/mjpeg/StreamsServlet.java
+++ src/au/edu/remotelabs/mjpeg/StreamsServlet.java
@@ -8,22 +8,28 @@
 package au.edu.remotelabs.mjpeg;
 
 import java.io.IOException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Streams
+ * Servlet implementation to serve MJpeg streams 
  */
-@WebServlet("/streams")
-public class Streams extends HttpServlet 
+@WebServlet(name="StreamsServlet",
+            urlPatterns = "/streams/*", 
+            initParams = { @WebInitParam(name = "config-file", value = "./META-INF/streams.xml") })
+
+public class StreamsServlet extends HttpServlet 
 {
     private static final long serialVersionUID = 1L;
 
-    public Streams() {
+    public StreamsServlet() 
+    {
         super();
     }
 
@@ -39,9 +45,17 @@ public class Streams extends HttpServlet
         response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException 
+    {
         // TODO Auto-generated method stub
         doGet(request, response);
+    }
+    
+    
+    public void destroy()
+    {
+        super.destroy();
     }
 
 }
