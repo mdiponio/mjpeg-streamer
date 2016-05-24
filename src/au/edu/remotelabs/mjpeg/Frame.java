c6352e099487118109f0a4150a974b8b53f01926
Michael Diponio <michael.diponio@uts.edu.au>
2016-05-24 12:21:22 +1000

    Started reading source.

---
+++ src/au/edu/remotelabs/mjpeg/Frame.java
@@ -0,0 +1,13 @@
+/**
+ * MJPEG streaming application.
+ * 
+ * @author Michael Diponio <michael.diponio@uts.edu.au>
+ * @date 18th May 2016
+ */
+
+package au.edu.remotelabs.mjpeg;
+
+public class Frame
+{
+
+}
--- src/au/edu/remotelabs/mjpeg/SourceStream.java
+++ src/au/edu/remotelabs/mjpeg/SourceStream.java
@@ -7,6 +7,8 @@
 
 package au.edu.remotelabs.mjpeg;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.util.logging.Logger;
 
 import au.edu.remotelabs.mjpeg.StreamerConfig.Stream;
@@ -19,8 +21,19 @@ public class SourceStream implements Runnable
     /** Stream configuration. */
     private final Stream config;
     
+    /** The boundary to discriminate between frames in M-Jpeg stream. This may
+     *  be configured but will be overridden if boundary is present in the response
+     *  HTTP header as is typically the case. */
+    private String boundary;
+    
     /** Whether the connection is being read from. */
-    private boolean isReading;
+    private boolean reading;
+    
+    /** Whether an error has occurred accessing the stream. */
+    private boolean errored;
+    
+    /** The last error that occurred. */
+    private String lastError;
     
     /** Thread which reads the stream. */
     private Thread readThread;
@@ -37,20 +50,90 @@ public class SourceStream implements Runnable
         {
             this.logger.info("Stream " + this.config.name + " is configured to continuously stream, attempting " +
                     "to connect at start up.");
-            
-            this.readThread = new Thread(this);
-            this.readThread.setName("Stream: " + this.config.name);
-            this.readThread.start();
+            this.start();
         }
+    }
+    
+    /**
+     * Starts the thread that reads the source stream.
+     * 
+     * @return true if able to start
+     */
+    public boolean start()
+    {
+        if (this.readThread != null && this.readThread.isAlive())
+        {
+            this.logger.warning("Unable to start stream because the reading thread for the stream is already running.");
+            return false;
+        }
+        
+        this.readThread = new Thread(this);
+        this.readThread.setName("Stream: " + this.config.name);
+        this.readThread.start();
+        
+        return false;
     }
 
     @Override
     public void run()
     {
+        this.reading = true;
         
-        
-        
-        
+        try
+        {
+            /* Open connection. */
+            HttpURLConnection conn = (HttpURLConnection) this.config.source.openConnection();
+            
+            /* Read boundary from content type header. */
+            String contentType = conn.getContentType();
+            if (contentType != null)
+            {
+                String boundaryTag = "boundary=";
+                int pos = contentType.indexOf(boundaryTag);
+                if (pos > 0)
+                {
+                    this.boundary = contentType.substring(pos + boundaryTag.length() + 1);
+                    this.logger.info("Loaded stream " + this.config.name + " boundary as " + boundary);
+                }
+            }
+            
+            
+            
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * Checks whether the source stream is being read.
+     * 
+     * @return true if reading source
+     */
+    public boolean isReading()
+    {
+        return this.reading;
+    }
+    
+    /**
+     * Checks whether there has been an error reading source stream.
+     * 
+     * @return true if reading error
+     */
+    public boolean isErrored()
+    {
+        return this.errored;
+    }
+    
+    /**
+     * Gets the error that occurred during reading source stream. 
+     * 
+     * @return reading error
+     */
+    public String getError()
+    {
+        return this.lastError;
+    }
 }
