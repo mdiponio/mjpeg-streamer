dd43f9602db3a45c5bb4025c7706f9e892445be4
Michael Diponio <michael.diponio@uts.edu.au>
2016-10-04 15:40:27 +1100

    Added crop operation.

---
+++ src/au/edu/remotelabs/mjpeg/dest/CropOp.java
@@ -0,0 +1,62 @@
+/**
+ * MJPEG streaming application.
+ * 
+ * @author Michael Diponio <michael.diponio@uts.edu.au>
+ * @date 28th September 2016
+ */
+
+package au.edu.remotelabs.mjpeg.dest;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Transformation operation which returns a region of the source frame.
+ */
+public class CropOp implements TransformOp
+{
+    /** Start offset x coordinate. */
+    private int offsetX;
+    
+    /** Start offset y coordinate. */
+    private int offsetY;
+    
+    /** Width of the region. */
+    private int width;
+    
+    /** Height of region. */
+    private int height;
+
+    @Override
+    public boolean configure(String param)
+    {
+        /* Input format is <x>,<y>,<width>,<height>. */
+        
+        String parts[] = param.split(",");
+        if (parts.length != 4) return false;
+        
+        this.offsetX = Integer.parseInt(parts[0]);
+        this.offsetY = Integer.parseInt(parts[1]);
+        this.width = Integer.parseInt(parts[2]);
+        this.height = Integer.parseInt(parts[3]);
+        
+        return true;
+    }
+
+    @Override
+    public BufferedImage apply(BufferedImage image) throws IOException
+    {
+        if (this.offsetX + this.width > image.getWidth())
+        {
+            this.width = image.getWidth() - this.offsetX;
+        }
+        
+        if (this.offsetY + this.height > image.getHeight())
+        {
+            this.height = image.getHeight() - this.offsetY;
+        }
+        
+        return image.getSubimage(this.offsetX, this.offsetY, this.width, this.height);
+    }
+
+}
